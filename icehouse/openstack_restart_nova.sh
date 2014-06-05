0305625b1ec124853b1b732f630371a11a74792e
Kord Campbell <kordless@gmail.com>
2014-06-05 15:07:52 -0700

    Old method no longer works, so manually restart each

--- icehouse/openstack_restart_nova.sh
+++ icehouse/openstack_restart_nova.sh
@@ -7,4 +7,12 @@ if [ "$(id -u)" != "0" ]; then
 fi
 
 # restart nova
-cd /etc/init.d/; for i in $( ls nova-* ); do sudo service $i restart; done
+service nova-api restart
+service nova-cert restart
+service nova-api restart
+service nova-conductor restart
+service nova-consoleauth restart
+service nova-network restart
+service nova-compute restart
+service nova-novncproxy restart
+service nova-scheduler restart
