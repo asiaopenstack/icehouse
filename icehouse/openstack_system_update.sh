0f0ec346c19d541c3ba69f803c01f416ebf13b7c
Kord Campbell <kordless@gmail.com>
2014-06-05 11:08:31 -0700

    Remove repo

--- icehouse/openstack_system_update.sh
+++ icehouse/openstack_system_update.sh
@@ -6,16 +6,6 @@ if [ "$(id -u)" != "0" ]; then
    exit 1
 fi
 
-# stuff canonical's repo in sources and update the system (for 12.04.1 only)
-if grep -q STACKGEEK /etc/apt/sources.list
-then
-  echo "Grizzly repo already added to /etc/apt/sources.list.  We're ready to rock."
-else
-  echo '# STACKGEEK ADDED THIS' >> /etc/apt/sources.list
-  echo 'deb http://ubuntu-cloud.archive.canonical.com/ubuntu precise-updates/grizzly main' >> /etc/apt/sources.list
-fi
-
-apt-get install ubuntu-cloud-keyring -y
 apt-get update -y
 apt-get install curl -y
 
