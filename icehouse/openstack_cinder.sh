03397d904ab4847ad49cb78809c33af8f92e6d8e
Kord Campbell <kordless@gmail.com>
2014-06-05 13:52:50 -0700

    Crazy config changes

--- icehouse/openstack_cinder.sh
+++ icehouse/openstack_cinder.sh
@@ -16,48 +16,39 @@ password=$SG_SERVICE_PASSWORD
 managementip=$SG_SERVICE_CONTROLLER_IP
 
 # install packages
-apt-get install -y iscsitarget iscsitarget-source
-apt-get install -y open-iscsi iscsitarget-dkms
+apt-get install -y lvm2
+apt-get install -y python-cinderclient
 apt-get install -y cinder-api cinder-scheduler cinder-volume 
 
 # edit keystone conf file to use templates and mysql
 if [ -f /etc/cinder/cinder.conf.orig ]; then
   echo "Original backup of cinder config files exist. Your current configs will be modified by this script."
-  cp /etc/default/iscsitarget.orig /etc/default/iscsitarget
-  cp /etc/cinder/api-paste.ini.orig /etc/cinder/api-paste.ini
   cp /etc/cinder/cinder.conf.orig /etc/cinder/cinder.conf
 else
-  cp /etc/default/iscsitarget /etc/default/iscsitarget.orig
-  cp /etc/cinder/api-paste.ini /etc/cinder/api-paste.ini.orig
   cp /etc/cinder/cinder.conf /etc/cinder/cinder.conf.orig
 fi
 
-# toggle iscsitarget
-sed -i 's/false/true/g' /etc/default/iscsitarget
-
-# hack up the cinder paste file
-sed -e "
-/^service_host =.*$/s/^.*$/service_host = $managementip/
-/^auth_host =.*$/s/^.*$/auth_host = $managementip/
-" -i /etc/cinder/api-paste.ini
-
-sed -e "
-s,127.0.0.1,$managementip,g;
-s,%SERVICE_TENANT_NAME%,service,g;
-s,%SERVICE_USER%,cinder,g;
-s,%SERVICE_PASSWORD%,$password,g;
-" -i /etc/cinder/api-paste.ini
-
- # hack up the cinder config file
 echo "
-iscsi_ip_address=$managementip
-sql_connection = mysql://cinder:$password@$managementip/cinder
+rpc_backend = cinder.openstack.common.rpc.impl_kombu
+rabbit_host = localhost
+rabbit_port = 5672
+rabbit_userid = guest
+rabbit_password = guest
+
+[database]
+connection = mysql:\/\/cinder:$password@$managementip\/cinder/
+
+[keystone_authtoken]
+auth_uri = http://$managementip:5000
+auth_host = $managementip
+auth_port = 35357
+auth_protocol = http
+admin_tenant_name = service
+admin_user = cinder
+admin_password = $password
 " >> /etc/cinder/cinder.conf
 
 # restart and sync
-service iscsitarget start
-service open-iscsi start
-sleep 5
 cinder-manage db sync
 
 # restart cinder services
