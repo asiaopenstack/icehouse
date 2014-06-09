7f787f2ca0079d2a54d6fe580437e5a786060f6f
Kord Campbell <kordless@gmail.com>
2014-06-09 15:42:00 -0700

    New compute rig config

--- icehouse/openstack_nova_compute.sh
+++ icehouse/openstack_nova_compute.sh
@@ -16,6 +16,10 @@ fi
 
 clear
 
+# grab our IP 
+read -p "Enter the device name for this rig's NIC (eth0, etc.) : " rignic
+rigip=$(/sbin/ifconfig $rignic| sed -n 's/.*inet *addr:\([0-9\.]*\).*/\1/p')
+
 # some vars from the SG setup file getting locally reassigned 
 password=$SG_SERVICE_PASSWORD    
 managementip=$SG_SERVICE_CONTROLLER_IP
@@ -59,37 +63,30 @@ api_paste_config=/etc/nova/api-paste.ini
 # RABBIT
 rabbit_host=$managementip
 rabbit_port=5672
+rpc_backend = nova.openstack.common.rpc.impl_kombu
+rabbit_userid=guest
+rabbit_password=guest
 
 # SCHEDULER
-compute_scheduler_driver=nova.scheduler.simple.SimpleScheduler
-scheduler_available_filters=nova.scheduler.filters.standard_filters
-scheduler_max_attempts=3
-scheduler_default_filters=AvailabilityZoneFilter,RamFilter,ComputeFilter,CoreFilter,SameHostFilter,DifferentHostFilter,RetryFilter
-least_cost_functions=nova.scheduler.least_cost.compute_fill_first_cost_fn
-default_availability_zone=nova
-default_schedule_zone=nova
+compute_scheduler_driver=nova.scheduler.filter_scheduler.FilterScheduler
 
 # NETWORK
 network_manager=nova.network.manager.FlatDHCPManager
-firewall_driver=nova.virt.libvirt.firewall.IptablesFirewallDriver
-multi_host=True
-public_interface=$computenic
-fixed_range=10.0.47.0/24
-dmz_cidr=10.128.0.0/24
-force_dhcp_release=true
-dns_server=8.8.8.8
-send_arp_for_ha=true
-auto_assign_floating_ip=false
-#dhcp_domain=geekceo.com
+force_dhcp_release=True
 dhcpbridge_flagfile=/etc/nova/nova.conf
 dhcpbridge=/usr/bin/nova-dhcpbridge
-libvirt_use_virtio_for_bridges=true
+firewall_driver=nova.virt.libvirt.firewall.IptablesFirewallDriver
+my_ip=$managementip
+public_interface=br100
+vlan_interface=$computenic
+flat_network_bridge=br100
+flat_interface=$computenic
 dnsmasq_config_file=/etc/nova/dnsmasq-nova.conf
-use_ipv6=true
 
 # GLANCE
 image_service=nova.image.glance.GlanceImageService
 glance_api_servers=$managementip:9292
+glance_host=$managementip
 
 # CINDER
 volume_api_class=nova.volume.cinder.API
@@ -98,19 +95,21 @@ snapshot_image_format=qcow2
 iscsi_helper=tgtadm
 
 # COMPUTE
+network_api_class = nova.network.api.API
+security_group_api = nova
 compute_manager=nova.compute.manager.ComputeManager
-sql_connection=mysql://nova:$password@$managementip/nova
 connection_type=libvirt
 compute_driver=libvirt.LibvirtDriver
 libvirt_type=kvm
 libvirt_inject_key=false
-rootwrap_config=/etc/nova/rootwrap.conf
+root_helper=sudo nova-rootwrap /etc/nova/rootwrap.conf
 remove_unused_base_images=true
 remove_unused_resized_minimum_age_seconds=3600
 remove_unused_original_minimum_age_seconds=3600
 checksum_base_images=false
 start_guests_on_host_boot=true
 resume_guests_state_on_host_boot=true
+volumes_path=/var/lib/nova/volumes
 
 # QUOTAS
 quota_security_groups=50
@@ -124,6 +123,7 @@ ram_allocation_ratio=1.5
 keystone_ec2_url=http://$managementip:5000/v2.0/ec2tokens
 
 # VNC CONFIG
+my_ip=$computeip
 novnc_enabled=true
 novncproxy_base_url=http://$managementip:6080/vnc_auto.html
 xvpvncproxy_base_url=http://$managementip:6081/console
@@ -136,6 +136,8 @@ vncserver_proxyclient_address=$computeip
 osapi_max_limit=1000
 
 # APIs
+enabled_apis=ec2,osapi_compute,metadata
+osapi_compute_extension = nova.api.openstack.compute.contrib.standard_extensions
 ec2_workers=4
 osapi_compute_workers=4
 metadata_workers=4
@@ -145,6 +147,19 @@ osapi_compute_listen_port=8774
 ec2_listen=$computeip
 ec2_listen_port=8773
 ec2_host=$computeip
+ec2_private_dns_show_ip=True
+
+[database]
+connection = mysql://nova:$password@$managementip/nova
+
+[keystone_authtoken]
+auth_uri = http://$managementip:5000
+auth_host = $managementip
+auth_port = 35357
+auth_protocol = http
+admin_tenant_name = service
+admin_user = nova
+admin_password = $password
 " > /etc/nova/nova.conf
 
 # restart
